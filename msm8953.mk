aed69c8a49b7167d97d5694c67c116600366dbe6
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    msm8953-common: Add libbt-vendor in device-common.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b

--- msm8953.mk
+++ msm8953.mk
@@ -63,6 +63,9 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    libbt-vendor
+
+PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
 
