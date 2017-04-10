4adc89b32225bef2b9ceb54f1f9a83b9b54f0278
Ruthwar Kumar Ambeer <ruthwar@codeaurora.org>
2017-04-10 15:41:38 +0530

    msm8953-common: Add background atel services in whitelist
    telephony services are white-listed who all need
    to freely run in the background. This is temporary
    work-around and some of the services will be
    removed from the list based on the feasibility.
    
    Change-Id: Ice11429b5b78d5786dc701251b507b7dac5745ee

--- msm8953.mk
+++ msm8953.mk
@@ -278,6 +278,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.nb.device.rc
 
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \
