5ee2a43f7308eafc8d904283e0684d73fde395c2
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-05-21 00:05:58 +0800

    msm8953-common: Add CONFIG_MACH_NUBIA_NX551J define for camera hal
    * this alow to use special code in kernel headers for nx551j (exclude
    code incompatible with nx551j stock blobs)

--- camera/QCamera2/stack/mm-jpeg-interface/Android.mk
+++ camera/QCamera2/stack/mm-jpeg-interface/Android.mk
@@ -9,6 +9,8 @@ LOCAL_CFLAGS+= -D_ANDROID_ -DQCAMERA_REDEFINE_LOG
 
 LOCAL_CFLAGS += -Wall -Wextra -Werror -Wno-unused-parameter
 
+LOCAL_CFLAGS += -DCONFIG_MACH_NUBIA_NX551J
+
 LOCAL_C_INCLUDES+= $(kernel_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 
