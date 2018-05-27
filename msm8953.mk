523540bb951a88cc7aed3dda311ab96f72892e16
Michael Bestas <mkbestas@lineageos.org>
2018-05-27 05:18:54 +0300

    msm8953-common: sensors: Convert to kuntao target
    Change-Id: Ic7de84be4f5706d7a1ca2f210819bf8606165974

--- msm8953.mk
+++ msm8953.mk
@@ -300,7 +300,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service.nubia
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
