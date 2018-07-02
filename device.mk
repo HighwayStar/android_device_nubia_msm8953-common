7a47295812f6b4e65c23fca7fae38c42f7ad8f2f
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-07-02 12:01:54 +0800

    nx551j: Add overlays matching stock rom
    * add framework-res overlays that matching values to stock rom V1.12
     nubia/NX551J/NX551J:6.0.1/MMB29M/eng.nubia.20180327.124015:user/release-keys
    * add power_profile.xml

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_AAPT_CONFIG := normal
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
