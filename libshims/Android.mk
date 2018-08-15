6d0084125987b95f445a44e568102b7c2b7be2f4
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-08-15 23:34:32 +0800

    msm8953-common: Enable oss camera HAL, drop shims
    * enable building of camera.msm8953 from source
    * drop unneeded shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -3,20 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    GraphicBuffer.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libui
-
-LOCAL_MODULE := libshim_camera
-LOCAL_MODULE_TAGS := optional
-LOCAL_MULTILIB := 32
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
     bionic/bionic_time_conversions.cpp \
     bionic/pthread_cond.cpp
 
--- msm8953.mk
+++ msm8953.mk
@@ -61,7 +61,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libshim_camera \
+    camera.msm8953 \
     libshim_libc \
     Snap
 
