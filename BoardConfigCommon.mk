801fc1c8dec4e6afc67ce22041337a34a00fb6ed
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    msm8953-common: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,6 +161,8 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USES_MKE2FS := true
 
 # Power
+TARGET_HAS_NO_WLAN_STATS := true
+TARGET_TAP_TO_WAKE_NODE := "/sys/board_properties/tpd_suspend_status"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Recovery
