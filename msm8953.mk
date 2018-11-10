df494a25265744eb478a729d7ef00229d8356a28
dianlujitao <dianlujitao@lineageos.org>
2018-11-10 15:11:10 +0800

    msm8953-common: Update GPS blobs from daisy
     * From: xiaomi/daisy/daisy_sprout:9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ:user/release-keys
    
    Change-Id: Ia1646ae0b8158c1b312221788c08427a754f3a36

--- msm8953.mk
+++ msm8953.mk
@@ -131,7 +131,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcurl \
     libgnss \
-    libgnsspps
+    libgnsspps \
+    libsensorndkbridge
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
