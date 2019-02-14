59b185b8309f3105802e3a3e900c22155a47ae10
Michael Bestas <mkbestas@lineageos.org>
2019-02-15 01:12:57 +0200

    msm8953-common: Update IMS/Peripheral manager/QMI/RIL from tissot
    * From xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.4.0.PDHMIXM:user/release-keys
    
    Change-Id: I9d06d86efc6c5fd245ee6db3900ee06e84313911

--- msm8953.mk
+++ msm8953.mk
@@ -294,6 +294,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
